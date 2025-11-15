bffbba221e7697b4d7fa440b4b3891b0c3c8aafc
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-11-14 23:00:53 -0300

    create instance with validation layers

--- src/vw_engine2.rs
+++ src/vw_engine2.rs
@@ -1,3 +1,4 @@
+use anyhow::bail;
 use ash::vk;
 
 pub struct VwEngine2 {
@@ -22,6 +23,8 @@ impl VwEngine2 {
 unsafe fn create_instance(entry: &ash::Entry) -> anyhow::Result<ash::Instance> {
     use ash::khr;
 
+    check_extension_support(&entry)?;
+
     let app_info = vk::ApplicationInfo::default()
         .application_name(c"VkWizard Application")
         .application_version(vk::make_api_version(0, 1, 0, 0))
@@ -35,13 +38,46 @@ unsafe fn create_instance(entry: &ash::Entry) -> anyhow::Result<ash::Instance> {
         khr::win32_surface::NAME.as_ptr(),
     ];
 
+    let mut enable_layers = vec![];
+    if cfg!(debug_assertions) {
+        enable_layers.push(c"VK_LAYER_KHRONOS_validation".as_ptr());
+        println!("Enabling validation layers. Please wait...");
+    }
+
     let instance_create_info = vk::InstanceCreateInfo::default()
         .application_info(&app_info)
-        .enabled_extension_names(&extension_names);
+        .enabled_extension_names(&extension_names)
+        .enabled_layer_names(&enable_layers);
 
     let instance = unsafe { entry.create_instance(&instance_create_info, None) }?;
 
     Ok(instance)
+}
+
+fn check_extension_support(entry: &ash::Entry) -> anyhow::Result<()> {
+    use ash::khr;
+
+    let extensions = unsafe { entry.enumerate_instance_extension_properties(None) }?;
+    let mut has_surface = false;
+    let mut has_win32_surface = false;
+
+    for ext in extensions {
+        if ext.extension_name_as_c_str()? == khr::surface::NAME {
+            has_surface = true;
+        }
+        if ext.extension_name_as_c_str()? == khr::win32_surface::NAME {
+            has_win32_surface = true;
+        }
+    }
+
+    if !has_surface {
+        bail!("Required extension khr_surface is not supported");
+    }
+    if !has_win32_surface {
+        bail!("Required extension khr_win32_surface is not supported");
+    }
+
+    Ok(())
 }
 
 impl Drop for VwEngine2 {
